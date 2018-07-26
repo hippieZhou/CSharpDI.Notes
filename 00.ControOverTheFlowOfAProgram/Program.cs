8dc9d4fab25ae8e394f00778487ee482cc51144a
hippieZhou <hippiezhou@outlook.com>
2018-07-26 19:03:38 +0800

    Add project files.

---
+++ 00.ControOverTheFlowOfAProgram/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.ControOverTheFlowOfAProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool continueExecution = true;
+            do
+            {
+                Console.Write("請輸入您的名字:");
+                var firstName = Console.ReadLine();
+
+                Console.Write("請輸入您的姓:");
+                var lastName = Console.ReadLine();
+
+                Console.Write("您是否要儲存? Y/N: ");
+
+                var wantToSave = Console.ReadLine();
+
+                if (wantToSave.ToUpper() == "Y")
+                    SaveToDB(firstName, lastName);
+
+                Console.Write("您是否要結束離開? Y/N: ");
+
+                var wantToExit = Console.ReadLine();
+
+                if (wantToExit.ToUpper() == "Y")
+                    continueExecution = false;
+
+            } while (continueExecution);
+        }
+
+        private static void SaveToDB(string firstName, string lastName)
+        {
+            //在這裡需要將輸入的姓、名，寫入到資料庫內
+            Console.WriteLine($"{lastName} {firstName} 已經寫入到資料庫內了");
+        }
+    }
+}
---
+++ 00.InversionTheFlowOfProgram/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _00.InversionTheFlowOfProgram
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void Save_Click(object sender, RoutedEventArgs e)
+        {
+            var firstName = FirstName.Text;
+            var lastName = LastName.Text;
+            SaveToDB(firstName, lastName);
+        }
+
+        private void Clear_Click(object sender, RoutedEventArgs e)
+        {
+            FirstName.Text = "";
+            LastName.Text = "";
+        }
+
+        private void SaveToDB(string firstName, string lastName)
+        {
+            //在這裡需要將輸入的姓、名，寫入到資料庫內
+            Message.Text = $"{lastName} {firstName} 已經寫入到資料庫內了";
+        }
+    }
+}
